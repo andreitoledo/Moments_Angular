0ae7f76f397d38dd9d7f083d3c5be244b813676a
andreitoledo <andreiltoledo@hotmail.com>
2023-07-18 18:03:53 -0300

    Inserindo dados no sistema

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+
+import { MomentService } from 'src/app/service/moment.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -8,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 export class NewMomentComponent implements OnInit {
   btnText = "Compartilhar !";
 
-  constructor() { }
+  constructor(private momentService: MomentService) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
+
+  async createHandler(moment : Moment){
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
+
+
+   }
 
 }
