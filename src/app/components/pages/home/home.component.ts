aaa1ce4dc9b0ff0a6a4072de33957130668dab9c
andreitoledo <andreiltoledo@hotmail.com>
2023-07-19 17:57:41 -0300

    Busca de dados no Angular - Lupa

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -5,8 +5,6 @@ import { environment } from 'src/environments/environment';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Observable } from 'rxjs';
 
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -17,6 +15,9 @@ export class HomeComponent implements OnInit {
   allMoments: Moment[] = []
   moments: Moment[] = []
   baseApiUrl = environment.baseApiUrl
+
+  faSearch = faSearch;
+  searchTerm: string = '';
 
   constructor(private momentService: MomentService) { }
 
@@ -33,6 +34,16 @@ export class HomeComponent implements OnInit {
       this.allMoments = items.data;
       this.moments = items.data;
     });
+
+  }
+
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) =>{
+      return moment.title.toLowerCase().includes(value)
+  });
   }
 
 }
